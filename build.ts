7f8293564642b8ad13c420136f6f6ad219ce86f2
LukasMurdock <lukas.murdock@gmail.com>
2022-08-29 09:09:56 -0400

    refactor

--- build.ts
+++ build.ts
@@ -1,13 +1,13 @@
 // deno run --allow-read --allow-env --allow-write build.ts
 
 import { copySync } from 'https://deno.land/std@0.153.0/fs/copy.ts';
+import { relative } from 'https://deno.land/std@0.153.0/path/win32.ts';
 
 import { getLiquidEngine } from './config.ts';
 
 const engine = getLiquidEngine();
-
 const currentDirectory = Deno.cwd();
-
+const pageDirectory = '/pages';
 const siteFolder = `${currentDirectory}/site`;
 const assetFolder = `${currentDirectory}/assets`;
 
@@ -19,15 +19,8 @@ if (await exists(siteFolder)) {
 // Copy assets into site folder
 copySync(assetFolder, `${siteFolder}`);
 
-const pages = Deno.readDirSync('./pages');
 // Render pages
-for (const page of pages) {
-	if (page.isFile) {
-		console.log('name: ' + page.name);
-		const text = await engine.renderFile(page.name, { title: 'Lukas Murdock' });
-		Deno.writeTextFile(`${siteFolder}/${page.name}`, text);
-	}
-}
+render();
 
 export async function exists(filePath: string) {
 	try {
@@ -39,5 +32,37 @@ export async function exists(filePath: string) {
 		}
 
 		throw err;
+	}
+}
+
+/**
+ * A recursive directory render.
+ */
+function render(relativeDirectory = pageDirectory) {
+	const directoryPath = currentDirectory + relativeDirectory;
+	const pages = Deno.readDirSync(directoryPath);
+
+	for (const page of pages) {
+		if (page.isFile) {
+			const sourcePath = `${directoryPath}/${page.name}`;
+			const destinationPath = `${siteFolder + relativeDirectory.replace(pageDirectory, '')}/${
+				page.name
+			}`;
+
+			console.log(page.name);
+			// console.group();
+			// console.log(`source: ${sourcePath}`);
+			// console.log(`destination: ${destinationPath}`);
+			// console.groupEnd();
+			engine
+				.renderFile(sourcePath, { title: 'Lukas Murdock' })
+				.then((text) => Deno.writeTextFile(destinationPath, text));
+		} else if (page.isDirectory) {
+			// Recursive directory render
+			const directoryBuildPath =
+				siteFolder + relativeDirectory.replace(pageDirectory, '') + `/${page.name}`;
+			const directoryPath = `${relativeDirectory}/${page.name}`;
+			Deno.mkdir(directoryBuildPath).then(() => render(directoryPath));
+		}
 	}
 }
--- dev.ts
+++ dev.ts
@@ -7,6 +7,7 @@ import * as fileServer from 'https://deno.land/std@0.153.0/http/file_server.ts';
 const port = 8080;
 
 const liquidEngine = getLiquidEngine();
+const currentDirectory = Deno.cwd();
 
 async function handler(request: Request): Promise<Response> {
 	const url = new URL(request.url);
@@ -21,24 +22,21 @@ async function handler(request: Request): Promise<Response> {
 	} else {
 		console.log({ path });
 		// return file path
-		return await fileServer.serveFile(request, `./assets/${path}`);
+		return await fileServer.serveFile(request, `${currentDirectory}/assets/${path}`);
 	}
 }
 
 async function getFileBasedRoute(pathname: string) {
 	const extension = pathname.split('.')[1] ?? 'html';
 
-	// if (extension !== 'html') {
-	//     return fileServer.serveFile(pathname)
-	// }
-
 	if (pathname === '/' || pathname == '/index.html') {
 		return await liquidEngine.renderFile('index', { title: 'First!' });
-		// return await Deno.readFile('./pages/index.html');
 	} else {
-		const route = pathname.split('/');
+		console.log({ pathname });
+		const [_, ...route] = pathname.split('/');
 		try {
-			return await liquidEngine.renderFile(`/assets/${route[1]}`, { title: 'First!' });
+			console.log({ route });
+			return await liquidEngine.renderFile(route.join('/'));
 		} catch {
 			console.log(`404: pathname: ${pathname}`);
 			return await liquidEngine.renderFile('404', { title: '404 | Page not found.' });
